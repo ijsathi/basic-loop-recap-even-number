fdd101df912b214a2fab39672f6a901a06dea9f2
unknown <ijsathi2001rs@gmail.com>
2023-10-10 13:54:33 +0600

    add three problem

---
+++ pb4-max.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <limits.h>
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int a, max = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        scanf("%d", &a);
+        if (a > max)
+        {
+            max = a;
+        }
+    }
+    printf("%d\n", max);
+    return 0;
+}
